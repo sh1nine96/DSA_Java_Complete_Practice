5fa42126f3ad0e1a9be9626754a446805df61223
Shubham Sharma <sh1nine96@gmail.com>
2022-09-04 02:39:27 +0530

    initial commit

---
+++ Starting_with_Java/First_Idea_Project/src/com/shubham/Main.java
@@ -0,0 +1,11 @@
+package com.shubham;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextInt());
+    }
+}
---
+++ Starting_with_Java/Input_Output_DataTypes/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        // System.out.println("Hello World!"); => will print Hello World!
+        // System.out.println(args[0]); => java Main "Hi" 3 "2nd index" it will print Hi
+        System.out.println(args[2]);
+        
+    }
+}
+
