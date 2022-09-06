63e2440b8fb32d6b6ba391637f665a3e43d89ed8
Shubham Sharma <sh1nine96@gmail.com>
2022-09-06 10:42:28 +0530

    basic programmes

--- Starting_with_Java/Conditionals_and_Loops/code/src/com/shubham/FibonacciNumber.java
+++ Starting_with_Java/Conditionals_and_Loops/code/src/com/shubham/FibonacciNumber.java
@@ -1,8 +1,26 @@
 package com.shubham;
 
+import java.util.Scanner;
+
 // find the nth fibonacci number
 public class FibonacciNumber {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the number for fibonacci sum to be calculated");
+        int n = input.nextInt();
+
+        int a = 0;
+        int b = 1;
+        int count = 2;
+
+        while(count <= n){
+            int temp = b;
+            b = b + a;
+            a = temp;
+            count++;
+        }
+        System.out.println("Your "+ n +"th fibonacci sum is: " + b);
 
     }
 }
